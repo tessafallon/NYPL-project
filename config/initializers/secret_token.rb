92f8cd415963a14dca7e868754435cc2844a5266
Kphillycat <keithwilliamsjr@gmail.com>
2014-02-17 16:04:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transcriot::Application.config.secret_key_base = 'e3bbf8021614d60288eb268245285870ba1d9b11b399b5aa5f22edc82f1c13ee2623cea61c7b5515692635bd07fbe70efa04b669d653ab22a1fe1d44545ee1ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transcriot::Application.config.session_store :cookie_store, key: '_transcriot_session'
