55559c4a035b5044d9f057122ddcc29375731875
contessa <tessa.fallon@gmail.com>
2014-02-18 17:22:18 -0500

    changed validations, seeded db

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -2,7 +2,7 @@ class Claim < ActiveRecord::Base
   # attr_accessible :claim_date, :incident_date, :resolution_date, :claim_number, :record_number, :incident_address, :incident_address_type, :total_claimed, :total_awarded
 
   validates :incident_address_type,
-    :inclusion => { :in => ["Home", "Workplace", "Other"],
+    :inclusion => { :in => ["Home", "Workplace", "Other", nil],
     :message => "%{value} is not one of the stated options of home, workplace, or other."}
 
     def claimant_name
