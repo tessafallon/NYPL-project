cf1e4c280935dad4a03c3005d7aa8fe583fac556
Kate <katepdonahue@gmail.com>
2014-02-19 15:22:40 -0500

    Get x and y coordinates on mouse up and mouse down thanks http://jsfiddle.net/strangeline/jxqpv/light/

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,11 +2,24 @@ $(document).ready( function() {
 
   var $img = $(".transcribe-image-holder");
 
-  $img.on("mousedown", function() {
-    console.log("hello");
+  function getMousePos(e, $img) {
+    var pos = {};
+    if(e) {
+      pos.x = e.pageX;
+      pos.y = e.pageY;
+    }
+    else {
+      pos.x = window.event.clientX + $img.scrollLeft - $img.clientLeft;
+      pos.y = window.event.clientY + $img.scrollTop - $img.clientTop;
+    }
+    return pos;
+  }
+
+  $img.on("mousedown", function(e) {
+    console.log(getMousePos(e));
   });
 
-  $img.on("mouseup", function() {
-    console.log("goodbye");
+  $img.on("mouseup", function(e) {
+    console.log(getMousePos(e));
   });
 });
