f5b4c4b793d3dc2efaab9bc3fd495cfafb484fb0
Kate <katepdonahue@gmail.com>
2014-02-26 14:26:08 -0500

    Seed database with damaged hats

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-claims = Claim.create
+claims = Claim.create([{}, {}])
+
+damages = Damage.create([{:description => "hat", :quantity => 2, :unit => nil, :total_cost => 12.0, :overcharge? => true, :claim_id => 1},
+                         {:description => "hat", :quantity => 1, :unit => nil, :total_cost => 2.0, :overcharge? => false, :claim_id => 2}])
