49dc28dc62e39bab854702825f75e8aeb5984abf
Kate <katepdonahue@gmail.com>
2014-02-26 14:40:00 -0500

    Make stubbed json object to make graph of in d3 file

---
+++ app/assets/javascripts/hat_graph.js
@@ -0,0 +1,17 @@
+var jsonHats = [
+  {
+    "description": "hat",
+    "quantity": 2,
+    "unit": null,
+    "total_cost": 12.0,
+    "overcharge?": true,
+    "claim_id": 1
+  },
+  {
+    "description": "hat",
+    "quantity": 1,
+    "unit": null,
+    "total_cost": 2.0,
+    "overcharge?": false,
+    "claim_id": 2
+  }];
