cae2dccfee52502e5eab7c28c8b50e1f71a0cd0e
Kate <katepdonahue@gmail.com>
2014-03-03 14:58:03 -0500

    Fix a couple spelling errors, get ready to parse the file in identify_claim method

--- app/models/hocr_layer.rb
+++ app/models/hocr_layer.rb
@@ -1,6 +1,12 @@
 class HocrLayer < ActiveRecord::Base
   belongs_to :image
   has_many :hocr_lines
-  has_many :claim_horc_layers
-  has_many :claims, :through => :claim_horc_layers
+  has_many :claim_hocr_layers
+  has_many :claims, :through => :claim_hocr_layers
+
+  def claim_association
+    file = File.open(self.filename)
+
+  end
+
 end
