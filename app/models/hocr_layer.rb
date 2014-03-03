a0ec42d3f1aa383692270463e44bc36e980be284
Kate <katepdonahue@gmail.com>
2014-03-03 16:14:50 -0500

    Separate responsibility for populating claim data and creating claim, start iterating this is now a class method

--- app/models/hocr_layer.rb
+++ app/models/hocr_layer.rb
@@ -6,14 +6,21 @@ class HocrLayer < ActiveRecord::Base
   has_many :claim_hocr_layers
   has_many :claims, :through => :claim_hocr_layers
 
-  def identify_claim
-    file = File.open("./public/ocr_files/#{self.filename}_ocr.txt", "r")
-    data = file.read
-    if /.*\(Record\sNo.\s(\d+),\sof\s1863\.\).*/.match(data)
-      record_info = /.*\(Record\sNo.\s(\d+),\sof\s1863\.\).*/.match(data)
-      debugger
-      record_number = record_info[1].to_i
+  def self.identify_claims
+    self.all.each do |hocr_layer|
+      file = File.open("./public/ocr_files/#{hocr_layer.filename}_ocr.txt", "r")
+      data = file.read
+      if /.*\(Record\sNo.\s(\d+),\sof\s1863\.\).*/.match(data)
+        # this is the start of a claim
+        record_info = /.*\(Record\sNo.\s(\d+),\sof\s1863\.\).*/.match(data)
+        current_record = record_info[1].to_i
+        current_claim = Claim.create(:record_number => current_record)
+      end
     end
+  end
+
+  def create_claim
+
   end
 
 end
