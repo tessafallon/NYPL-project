d7ec8b12687373353b3d16bb505fc4da0f275977
Kate <katepdonahue@gmail.com>
2014-03-03 15:42:23 -0500

    Run ocr again to get text files, save to public folder, we'll parse these to identify the claim

--- app/models/hocr_layer.rb
+++ app/models/hocr_layer.rb
@@ -1,12 +1,15 @@
+require 'debugger'
+
 class HocrLayer < ActiveRecord::Base
   belongs_to :image
   has_many :hocr_lines
   has_many :claim_hocr_layers
   has_many :claims, :through => :claim_hocr_layers
 
-  def claim_association
-    file = File.open(self.filename)
-
+  def identify_claim
+    debugger
+    file = File.open("./public/hocr_files/#{self.filename}.html", "r")
+    puts "hi"
   end
 
 end
