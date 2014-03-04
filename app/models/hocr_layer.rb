99e198165b7dc0ee8f397a460aab1be768d1a3c7
Kate <katepdonahue@gmail.com>
2014-03-04 11:13:30 -0500

    Add regex to grab claimants name

--- app/models/hocr_layer.rb
+++ app/models/hocr_layer.rb
@@ -20,14 +20,29 @@ class HocrLayer < ActiveRecord::Base
         current_claim = Claim.create(:record_number => current_record)
       end
       if current_claim
-        hocr_layer.create_claim(current_claim)
+        hocr_layer.create_claim(current_claim, data)
       end
     end
   end
 
-  def create_claim(claim)
+  def create_claim(claim, data)
+    # grab name
+    claimant = /.*CLAIM\sOF\s(\w+\s+\w+)\.\W*\(Record\sNo\.\s\d+,\sof\s1863\.\).*/.match(data)
+    if claimant
+      claim.people << Person.create(:name => claimant[1].name_caps, :role => "claimant")
+    end
     # create association between hocr_layer and claim
     # populate claim data via regex on hocr_layer
+  end
+
+end
+
+class String
+
+  def name_caps
+    array = []
+    self.downcase!.split(" ").map {|name| array<<name[0].upcase+name[1..-1]}
+    array.join(" ")
   end
 
 end
