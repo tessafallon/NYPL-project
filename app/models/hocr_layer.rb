2867ae96b10130769e93fdf6423ec1451b941002
Kphillycat <keithwilliamsjr@gmail.com>
2014-03-05 17:19:14 -0500

    Add image to edit page

--- app/models/hocr_layer.rb
+++ app/models/hocr_layer.rb
@@ -38,7 +38,7 @@ class HocrLayer < ActiveRecord::Base
   # links site version of image at the top of the hocr_layer file
   def add_image(data)
     regex = /(?<=ppageno\s0'>)/
-    data.gsub(regex) {"\n<img src='./page_images/#{self.filename}.jpg'>"}
+    data.gsub(regex) {"\n<img src='/page_images/#{self.filename}.jpg'>"}
   end
 
   # identifies start of a claim and creates new claim object for each record number
